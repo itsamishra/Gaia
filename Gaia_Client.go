05e560bfa40c37797b1e37573205f44df52997c2
Ash <ashutoshmishra413@gmail.com>
2019-03-14 17:29:31 -0700

    Initial commit

---
+++ Gaia_Client.go
@@ -0,0 +1,24 @@
+package main
+
+import "net"
+import "fmt"
+import "bufio"
+import "os"
+
+func main() {
+	//listenPort1 := "127.0.0.1:8081"
+	listenPort2 := "35.243.155.9:8081"
+	// connect to this socket
+	conn, _ := net.Dial("tcp", listenPort2)
+	for {
+		// read in input from stdin
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Text to send: ")
+		text, _ := reader.ReadString('\n')
+		// send to socket
+		fmt.Fprintf(conn, text+"\n")
+		// listen for reply
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("Message from server: " + message)
+	}
+}
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var message = "Hello World!"
+
+	fmt.Println(message)
+
+	for i := 0; i<5; i++{
+		fmt.Println(i)
+	}
+}
